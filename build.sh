fc25b428d1d7d3140c1bbe962207fbed936be81e
Benno Rice <benno@jeamland.net>
2025-04-25 12:36:45 +1000

    Rename Python script so pip install . works

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 src/processor.py
+python3 src/aec2025.py
 npx @tailwindcss/cli -i css/input.css -o output/site.css
 tsc src/*.ts --lib es2015,dom --outDir output
 cp -vR img output
