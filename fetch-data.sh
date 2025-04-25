04960bdb9ca64cde4d2c09a759be86a172cca363
Benno Rice <benno@jeamland.net>
2025-04-25 14:07:49 +1000

    Make these fetch if-not-modified-since

--- fetch-data.sh
+++ fetch-data.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-SENATE_URL="https://www.aec.gov.au/election/files/data/senate-candidates.csv"
-REPS_URL="https://www.aec.gov.au/election/files/data/house-candidates.csv"
+DATA_URL="https://www.aec.gov.au/election/files/data"
+SENATE_FILENAME="senate-candidates.csv"
+REPS_FILENAME="house-candidates.csv"
 
 mkdir -p data
 
 cd data
 
-curl -LO ${SENATE_URL}
-curl -LO ${REPS_URL}
+for filename in "${SENATE_FILENAME}" "${REPS_FILENAME}"; do
+    curl -sLO -z "${filename}" "${DATA_URL}/${filename}"
+done
 
 cd ..
